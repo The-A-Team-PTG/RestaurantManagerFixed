2d037460a16a2f7036a3eb3182a79dcfbe71a2bd
dimitar-13 <65706772+dimitar-13@users.noreply.github.com>
2021-03-30 15:54:28 +0300

    Serve Meal method

---
+++ RestaurantManager/RestaurantManager/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager
+{
+    public class Product
+    {
+        public string name { get; private set; }
+
+        public double quantity { get;  set; }
+
+
+        public Product(string name , double  quantity)
+        {
+            this.name = name;
+            this.quantity = quantity;
+
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var manager = new RestaurantManager();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                string[] cmdParts = line.Split(" ");
+                string cmd = cmdParts[0];
+                switch (cmd)
+                {
+                    case "ServeMeal":
+                        string name = cmdParts[1];
+                        List<Product> product = new List<Product>();
+                        for (int i = 2; i < cmdParts.Length - 1; i += 2)
+                        {
+                            string productname = cmdParts[i];
+                            double productQuantity = double.Parse(cmdParts[i + 1]);
+                            product.Add(new Product(productname, productQuantity));
+                        }
+                        string date = cmdParts[cmdParts.Length - 1];
+                        string result = manager.ServeMeal(name, product, date);
+                        Console.WriteLine(result);
+                        break;
+                    default:
+                        break;
+                }
+
+                
+
+                }
+        }
+    }
+}
